0fbff80b488d93990d787f0cdaa5fe00e53dd6e8
Eduardo <edward.axl3@gmail.com>
2020-02-05 19:04:48 -0500

    Integrando plantilla adminlte3 3

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Advanced form elements</title>
+  <title>{{config ('app.name')}}</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -30,7 +30,7 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body class="sidebar-mini accent-dark sidebar-collapse control-sidebar-slide-open text-sm">
+<body class="sidebar-mini accent-dark control-sidebar-slide-open text-sm">
 <div class="wrapper">
   <!-- Navbar -->
   @include('includes.panel.nav')
