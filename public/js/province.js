99011c14c46d23a6edfef0ea3c20e0fe1745877f
Eduardo <echuquillanquiy@gmail.com>
2020-05-21 15:20:12 -0500

    modificando select dependientes

--- public/js/province.js
+++ public/js/province.js
@@ -11,7 +11,7 @@ function onSelectDepartmentChange()
     $.get('/api/department/'+department_id+'/provinces', function (data) {
         var html_select = '<option value="">[PROVINCIAS]</option>';
         for (var i = 0; i < data.length; ++i)
-            html_select += '<option value="'+data[i].id+'">'+data[i].id+'</option>';
+            html_select += '<option value="'+data[i].id+'">'+data[i].name+'</option>';
             $('#select-province').html(html_select);
     });
 }
@@ -29,7 +29,7 @@ function onSelectProvinceChange()
     $.get('/api/province/'+province_id+'/district', function (data2) {
         var html_select2 = '<option value="">[DISTRITOS]</option>';
         for (var i = 0; i < data2.length; ++i)
-            html_select2 += '<option value="'+data2[i].id+'">'+data2[i].id+'</option>';
+            html_select2 += '<option value="'+data2[i].id+'">'+data2[i].name+'</option>';
             $('#select-district').html(html_select2);
     });
 }
